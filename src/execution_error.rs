da0856fa90c89441792c61a0e94109bb4eb40a80
luka1104 <skyward.luke11@gmail.com>
2022-08-01 02:09:25 -0700

    execution_error rs

---
+++ src/execution_error.rs
@@ -0,0 +1,21 @@
+use crate::opcodes::Opcode;
+use std::fmt::{Display, Formatter, Result};
+
+#[derive(Debug, Eq, PartialEq)]
+pub enum ExecutionError {
+    StackOverflow,
+    StackUnderflow,
+    InvalidJump,
+    Result,
+    InvalidOpcode,
+    OutOfGas,
+    UnsupportedOpcode(Opcode),
+}
+
+impl Display for ExecutionError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result {
+        write!(f, "{:?}", self)
+    }
+}
+
+impl std::error::Error for ExecutionError {}
