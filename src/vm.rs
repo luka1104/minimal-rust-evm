c74ea7a5730f9c96bd9b5a563eb7c36817dafbb9
luka1104 <skyward.luke11@gmail.com>
2022-08-01 01:39:17 -0700

    vm rs

---
+++ src/vm.rs
@@ -0,0 +1,27 @@
+use crate::memory::Memory;
+use crate::stack::Stack;
+use ethereum_types::U256;
+use std::collections::HashMap;
+
+#[derive(Debug)]
+pub struct VmState {
+    pub pc: usize,
+    pub stack: Stack,
+    pub memory: Memory,
+    pub return_data: Vec<u8>,
+    pub storage: HashMap<U256, U256>,
+}
+
+const INITIAL_MEMORY_CAPACITY: usize = 0x40 + 32;
+
+impl VmState {
+    pub fn new() -> VmState {
+        VmState {
+            pc: 0,
+            stack: Stack::with_capacity(16),
+            memory: Memory::with_capacity(INITIAL_MEMORY_CAPACITY),
+            return_data: Vec::new(),
+            storage: HashMap::new(),
+        }
+    } 
+}
